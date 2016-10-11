9b6a753c56666e400e06229e61e16df762850f25
Xu Lei <a871460434@gmail.com>
2016-10-11 21:11:54 +0800

    Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import '../App.css';
+import Header from './Header';
+import Gallery from './Gallery';
 class Main extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Header />
+        <Gallery />
       </div>
     );
   }
