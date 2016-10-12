b6374e6df10328342dd3156326509f14aa12508d
Xu Lei <a871460434@gmail.com>
2016-10-12 20:49:42 +0800

    add header and footer

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Header = () => (
+  <header >Choose your Favourite</header>
+);
+export default Header;
---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+const footer = () => (
+      <footer >
+        This is the footer.
+      </footer>
+);
+export default footer;
