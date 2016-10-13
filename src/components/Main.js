dc2ada54debf1f253c1cc9fc21400efe841acf53
Xu Lei <a871460434@gmail.com>
2016-10-13 22:48:48 +0800

    add footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import '../App.css';
 import Header from './Header';
 import Gallery from './Gallery';
+import Footer from './footer';
 class Main extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <Gallery />
+        <Footer />
       </div>
     );
   }
