b5dff9edfb1bb6533fe3fb506c94e5c582ee9ef3
Xu Lei <a871460434@gmail.com>
2016-10-13 22:50:58 +0800

    add react-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
+import Photo from './components/photos';
+import Category from './components/category';
+import NotFound from './components/NotFound';
+import { BrowserRouter , Match,Miss} from 'react-router';
 import './styles/index.css';
 import './styles/font-awesome/css/font-awesome.min.css';
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={Main}/>
+        <Match pattern="/photo/:photoId" component={Photo}/>
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
 ReactDOM.render(
-  <Main />,
+  <Root />,
   document.getElementById('root')
 );
