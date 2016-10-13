a181dbbe69f7568ed62748070d61f3a5ef887796
Xu Lei <a871460434@gmail.com>
2016-10-13 22:51:29 +0800

    category component

---
+++ src/components/category.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+class Category extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <section>
+      <figcaption>
+        <div className="info">
+          <h2>{this.props.title}</h2>
+          <img src={this.props.cover} />
+          <i className="fa fa-search-plus" aria-hidden="true"><Link to={{
+            pathname:'photo/'+this.props.id,
+            query: { id : this.props.id }
+          }}>To</Link></i>
+        </div>
+      </figcaption>
+      </section>
+    );
+  }
+}
+export default Category;
