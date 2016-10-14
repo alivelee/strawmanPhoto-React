7c9abc51f5ef335bb616f87fe715a8a5a1b4ffd5
Xu Lei <a871460434@gmail.com>
2016-10-14 22:58:03 +0800

    firebase

---
+++ src/components/firebase.js
@@ -0,0 +1,7 @@
+import Rebase from 're-base';
+const Firebase = Rebase.createClass({
+  apiKey: "AIzaSyBKn5tw9la6jNkrEsNhHrVdNRpRJm0xLFs",
+  authDomain: "strawmanphoto.firebaseapp.com",
+  databaseURL: "https://strawmanphoto.firebaseio.com"
+});
+export default Firebase;
