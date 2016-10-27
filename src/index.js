f82d9214274d721ec646b2a1645bddd2a8419052
Xu Lei <a871460434@gmail.com>
2016-10-27 23:03:58 +0800

    import main entry component and match a router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
+import MainGallery from './components/MainGallery';
 import Photo from './components/photos';
-import Category from './components/category';
 import NotFound from './components/NotFound';
 import Devices from './components/Devices';
 import { BrowserRouter , Match,Miss} from 'react-router';
@@ -13,6 +13,7 @@ const Root = () => {
     <BrowserRouter>
       <div>
         <Match exactly pattern="/" component={Main}/>
+        <Match exactly pattern="/gallery" component={MainGallery} />
         <Match pattern="/photo/:photoId" component={Photo}/>
         <Match pattern="/devices" component={Devices} />
         <Miss component={NotFound}/>
