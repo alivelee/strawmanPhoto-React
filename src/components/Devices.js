70d2eb044ebe8ad51a19ca00421dc76881a9a93a
Xu Lei <a871460434@gmail.com>
2016-10-27 23:03:00 +0800

    add device component

---
+++ src/components/Devices.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Firebase from './firebase';
+import Header from './Header';
+import DevicesList from './DevicesList';
+class Devices extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[]
+    }
+  }
+  componentWillMount(){
+    Firebase.syncState('Devices', {
+        context: this,
+        state: 'items',
+        asArray: true
+      });
+    }
+  componentWillUnmount() {
+    Firebase.removeBinding(this.ref)
+  }
+  render(){
+    return (
+      <div>
+        <Header />
+        <DevicesList items={this.state.items} />
+      </div>
+    )
+  }
+
+}
+export default Devices;
