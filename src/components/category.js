38f999d8868d062cfee394fb58f7da12908dc89e
Xu Lei <a871460434@gmail.com>
2016-10-28 23:33:36 +0800

    change section to div
    avoid css style affect each other

--- src/components/category.js
+++ src/components/category.js
@@ -6,20 +6,20 @@ class Category extends Component {
   }
     render(){
     return (
-      <section>
-      <figcaption>
-        <div className="info">
-          <h2>{this.props.title}</h2>
-          <img src={this.props.cover} />
-          <i className="fa fa-search-plus" aria-hidden="true"><Link to={{
-            pathname:'photo/'+this.props.id,
-            query: {
-              id : this.props.id,
-            }
-          }}>To</Link></i>
-        </div>
-      </figcaption>
-      </section>
+      <div className="category">
+        <figcaption>
+          <div className="info">
+            <h2>{this.props.title}</h2>
+            <img src={this.props.cover} />
+            <i className="fa fa-search-plus" aria-hidden="true"><Link to={{
+              pathname:'photo/'+this.props.id,
+              query: {
+                id : this.props.id,
+              }
+            }}>To</Link></i>
+          </div>
+        </figcaption>
+    </div>
     );
   }
 }
